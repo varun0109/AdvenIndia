add2e4001d9d7afb10021c5c157b4751944b6016
varun0109 <37083484+varun0109@users.noreply.github.com>
2024-12-04 18:44:03 +0530

    Add files via upload

---
+++ Eshop77/store/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models.product import Product
+from .models.category import Category
+from .models.customer import Customer
+from .models.orders import Order
+from .models.addfavourites import favourite
+from .models.seller import Seller
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category , AdminCategory)
+admin.site.register(Customer )
+admin.site.register(Order )
+admin.site.register(favourite )
+admin.site.register(Seller )
